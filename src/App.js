e0520667169f07580e0c4619b593df712707e821
KyleFC <35385929+KyleFC@users.noreply.github.com>
2024-02-22 02:53:48 -0800

    Hopefully fixed dependencies for the last time

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import './App.css'; // your custom styles
 
 function App() {
