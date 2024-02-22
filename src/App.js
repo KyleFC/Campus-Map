1d43e8dbe0d59d3e69b070ce311488bd967b1203
KyleFC <35385929+KyleFC@users.noreply.github.com>
2024-02-22 02:31:21 -0800

    fixed errors

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import SearchBox from './components/SearchBox';
-import Parameters from './components/Parameters';
-import Legend from './components/Legend';
 import './App.css'; // your custom styles
 
 function App() {
   return (
     <div className="app">
-      <Parameters />
       <div className="map-wrapper">
         <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '100vh', width: '100%' }}>
           <TileLayer
@@ -19,8 +15,6 @@ function App() {
           />
           {[17, 33.65348, -117.81032]}
         </MapContainer>
-        <SearchBox />
-        <Legend />
       </div>
     </div>
   );
