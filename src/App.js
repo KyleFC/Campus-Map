296babb20e114fc78e7ba6eba27ae9277682b64b
KyleFC <35385929+KyleFC@users.noreply.github.com>
2024-02-22 02:27:27 -0800

    app.js template code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js or your main component file
+
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import SearchBox from './components/SearchBox';
+import Parameters from './components/Parameters';
+import Legend from './components/Legend';
+import './App.css'; // your custom styles
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Parameters />
+      <div className="map-wrapper">
+        <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '100vh', width: '100%' }}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {[17, 33.65348, -117.81032]}
+        </MapContainer>
+        <SearchBox />
+        <Legend />
+      </div>
     </div>
   );
 }
